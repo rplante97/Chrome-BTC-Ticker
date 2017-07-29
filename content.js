e12f00007703fb3f0517f5b43b6dcd813fee7f9a
Ryan Plante <rplante97@gmail.com>
2017-07-29 19:51:42 -0400

    Updated ticker behavior
    -Ticker now displays floating point values
    
    -When price fetched is more than last price background turns green for 5 seconds, conversely if price fetched is less than last price background changes to red

--- content.js
+++ content.js
@@ -1,26 +1,43 @@
 //Initialize variables
 const xhr = new XMLHttpRequest();
-let amt = 0;
+var oldAmount = null;
 
 //Coinbase price data url for BTC-USD
 const COINBASE_BTC_PRICE_URL = "https://api.coinbase.com/v2/prices/BTC-USD/spot"
 
 //Set icon badge with current price of BTC
 const setBadge = (amt) => {
+  if(amt > oldAmount){
+  	//green (5 seconds)
+  	chrome.browserAction.setBadgeBackgroundColor({color: "#008000"});
+  }
+  else if (amt < oldAmount){
+  	//red (5  seconds)
+  	chrome.browserAction.setBadgeBackgroundColor({color: "#FF0000"});
+  }
+  setTimeout(setColorDefault, 5000);
+
   chrome.browserAction.setBadgeText({
-    text: amt.split('.')[0]
+    text: amt
   });
+  oldAmount = amt;
 };
+
+const setColorDefault = function() {
+	chrome.browserAction.setBadgeBackgroundColor({color: "#3297FD"});
+}
 
 //Fetches current BTC price and sets badge
 const fetchPrice = () => {
   xhr.open("GET", COINBASE_BTC_PRICE_URL, true);
   xhr.onreadystatechange = function() {
+  	//readyState 4 means request is finished and response is ready
     if (xhr.readyState === 4) {
-      const resp = JSON.parse(xhr.responseText);
-      const { data: { amount } } = resp;
-      amt = amount;
-      setBadge(amount);
+      const response = JSON.parse(xhr.responseText);
+      const { data: { amount } } = response;
+      var amt = parseFloat(amount);
+      amt = amt.toFixed(1);
+      setBadge(amt);
     }
   }
   xhr.send();
@@ -30,4 +47,4 @@ const fetchPrice = () => {
 fetchPrice();
 
 //Poll for price every 10 seconds
-setInterval(fetchPrice, 10000);
+setInterval(fetchPrice, 5000);
